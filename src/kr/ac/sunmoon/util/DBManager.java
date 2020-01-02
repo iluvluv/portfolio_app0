7e7983f536af4641c4eae2c054b95c9cf9b12d3e
Administrator <somin0416@naver.com>
2020-01-02 16:26:55 +0900

    init.

---
+++ src/kr/ac/sunmoon/util/DBManager.java
@@ -0,0 +1,52 @@
+package kr.ac.sunmoon.util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBManager {
+	private static DataSource datasource;
+	static{
+		Context context;
+		try {
+			context = (Context) new InitialContext().lookup("java:comp/env/");
+			datasource = (DataSource) context.lookup("jdbc/mysql");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//DB클래스가 자동적으로 실행 될때 한번 실행
+		
+	}
+	
+	public static Connection getConnection() throws Exception {
+		
+			return datasource.getConnection();
+		
+	}
+	
+	public static void close(ResultSet rs, Statement stmt, Connection con) {
+
+		try {
+			if(rs != null ) { rs.close(); }
+			if(stmt != null ) { stmt.close(); }
+			if(con != null ) { con.close(); }
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void close(Statement stmt, Connection con) {
+		
+		try {
+
+			if(stmt != null ) { stmt.close(); }
+			if(con != null ) { con.close(); }
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
